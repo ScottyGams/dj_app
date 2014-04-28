4a546d2664773fb4be10faa8359a7af6ea0ea1be
ScottyGams <ScottyGammon@gmail.com>
2014-04-28 14:04:45 +0100

    Initial DJ App Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DjApp::Application.config.secret_token = 'a71f1e9795780012027999ec3fc680af2149698cee301bf95016c7fed93320589e49341ce029dbe095edcc2276e4c679fbdf48a08396e2e091314e3793e1b5cd'
